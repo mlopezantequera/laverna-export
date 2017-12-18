ec6fa7210ee944925a1771526c4cff45df960e20
Manuel Lopez Antequera <mlopezantequera@gmail.com>
2017-12-18 17:37:56 +0100

    Works with json+md export format plus:
    + tags included in filename and in the notes
    + creation and modification names respected

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 #Intro
-This python script exports notes from [Laverna](http://laverna.cc) to markdown files by extracting the content from the JSON files that Laverna uses to store notes.
+This python script exports notes from [Laverna](http://laverna.cc) to markdown files.
 The notes are saved in directories corresponding to notebooks.
 
-#Usage
-Once you have the directory of JSON files ready to go, edit the directory locations at the top of the script and specify an output directory.
-Then run the script:
+This version is updated to work with the new laverna export format (.json + 'incomplete'.md)
 
-```$ python export.py```
+#Usage
+
+./export.py path_to_laverna_backups output_path
+
+laverna_backups is the directory that you find in the laverna exported .zip file
+output_path is the desired path for your .md files.
 
 #Limitations
 This is a simple script that has some limitations. However, it could easily be extended to handle more complex use cases.
 
 - Flat notebook structure: the exporter ignores the notebook hierarchy
-- Ignores other fields: only `title`, `content`, and `notebookId` are used, so
-  `tags` are ignored, for example
+  
--- export.py
+++ export.py
@@ -1,48 +1,87 @@
 #Python 3
 
-import re,os,json,glob
+"""
+Updated version of https://github.com/tobanw/laverna-export
 
-workdir = '/home/toban/documents/code/project/json-md-exporter'
-laverndir = 'Laverna-tw'
-dirnotebooks = 'notebooks'
-dirnotes = 'notes'
-outdir = 'md-output'
+Upgrades:
+
+- Works with new laverna export format (.json + 'incomplete'.md)
+- Tags are included in each note and in the filename (compatible with tagspaces)
+- Creation and modification times are kept
+
+Usage:
+
+./export.py path_to_laverna_backups output_path
+
+laverna_backups is the directory that you find in the laverna exported .zip file
+output_path is the desired path for your .md files.
+"""
+
+import re,os,json,glob,sys
 
 def main():
+
+    workdir = sys.argv[1] 
+    out_dir = sys.argv[2] # example: '/home/user/laverna-exported-md'
+
+    db_dir = 'notes-db'
+    notes_dir = 'notes'
+
     try:
-        os.mkdir(os.path.join(workdir,outdir))
+        os.mkdir(out_dir)
     except FileExistsError:
-        print('Output directory already exists.')
-
-    for notebookjson in glob.iglob(os.path.join(workdir,laverndir,dirnotebooks,'*.json')):
-        #import json file as dict
-        with open(notebookjson,'r') as filestr:
-            notebookdict = json.load(filestr)
-
-        #create dir for notebook
-        nbdirname = clean_dirname(notebookdict['name'].lower())
-        outpath = os.path.join(workdir,outdir,nbdirname)
+        print('Output directory already exists. Proceeding')
+      
+    # we will save each notebook in a subfolder (not tested)
+    notebooksjson = os.path.join(workdir, db_dir, 'notebooks.json')
+    with open(notebooksjson,'r') as f:
+      notebooks = json.load(f)
+    notebook_names = {}  
+    for notebook in notebooks:
+        notebook_names['id'] = notebook['name']
         try:
-            os.mkdir(outpath)
+          os.mkdir(out_dir, notebook['name'])
         except FileExistsError:
-            print('Notebook output directory already exists.')
+          pass
 
-        #process each notebook to own dir
+    for notejson in glob.iglob(os.path.join(workdir,db_dir,notes_dir,'*.json')):
+        notemd = notejson.replace('.json', '.md')
+        
+        with open(notejson,'r') as f:
+          notedict = json.load(f)
+                  
+        with open(notemd,'r') as f:
+          notetext = f.read()
+          
+        # we will save each notebook in a subfolder (not tested)
+        if notedict['notebookId'] in notebook_names.keys():
+            notebook_dir = notebook_names['notebookId']
+        else:
+            notebook_dir = ''
 
-        for notejson in glob.iglob(os.path.join(workdir,laverndir,dirnotes,'*.json')):
-            with open(notejson,'r') as notestr:
-                notedict = json.load(notestr)
+        # create filename. It will include tags if they exist
+        notename = clean_filename(notedict['title'].lower(), 'md')
+        if len(notedict['tags']) > 0:
+            tagstr = ' '.join(notedict['tags'])
+            notepath = os.path.join(out_dir,notebook_dir,notename.replace('.md','['+tagstr+'].md'))
+        else:
+            notepath = os.path.join(out_dir,notebook_dir,notename)
 
-            #if note belongs to notebook
-            if notedict['notebookId'] == notebookdict['id']:
-                notename = clean_filename(notedict['title'].lower(), 'md')
-                notepath = os.path.join(outpath,notename)
+        # copy laverna's markdown. Add title and tags.
+        with open(notepath,'w') as output_md:
+            output_md.write('#'+notedict['title']+'\n\n')
+            if len(notedict['tags']) > 0:
+              output_md.write('tags: ')             
+              for tag in notedict['tags']:
+                output_md.write('['+tag+'] ')
+              output_md.write('\n\n')
+            output_md.write(notetext)
                 
-                #create file, add title header and content
-                with open(notepath,'w') as notemd:
-                    notemd.write('#{}\n\n{}'.format(notedict['title'],notedict['content']))
-
-    print('Done!')
+        #fix creation and modification date
+        os.utime(notepath, (notedict['created'] / 1000, notedict['updated'] / 1000))        
+        
+        print(notepath, 'done.')
+    print('All Done!')
 
 #filename utility functions
 def clean_filename(namestr,ext):
@@ -50,12 +89,6 @@ def clean_filename(namestr,ext):
     cleaned = sep_to_dash(cleaned)
     cleaned = strip_special(cleaned)
     return cleaned + '.' + ext
-
-def clean_dirname(namestr):
-    cleaned = strip_ends(namestr)
-    cleaned = sep_to_dash(cleaned)
-    cleaned = strip_special(cleaned)
-    return cleaned
 
 def strip_ends(namestr):
     return re.sub('^\W+|\W+$','',namestr)
